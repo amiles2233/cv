79139454f4809ab313f2fbeff308a0f1d07ea267
Aaron <amiles2233@gmail.com>
2020-10-05 11:50:19 -0700

    Update CV to reflect SD experience

--- render_cv.r
+++ render_cv.r
@@ -8,7 +8,7 @@
 # Knit the HTML version
 rmarkdown::render("cv.rmd",
                   params = list(pdf_mode = FALSE),
-                  output_file = "aaron_miles_cv.html")
+                  output_file = "aaron_miles_cv_01.html")
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
@@ -18,4 +18,4 @@ rmarkdown::render("cv.rmd",
 
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input =tmp_html_cv_loc,
-                       output = "aaron_miles_cv.pdf")
+                       output = "aaron_miles_cv_01.pdf")
